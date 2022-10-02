c146e6ba891c0874571f7a85669c9b4eb48382c0
Casper-NS <caspernyvang@gmail.com>
2022-10-02 19:43:55 +0200

    minor update

--- src/projectile.rs
+++ src/projectile.rs
@@ -1,8 +1,10 @@
-/*#[derive(Default)]
+#[derive(Default)]
 pub struct Point{
-    x: f64,
-    y: f64
-}*/
+    pub x: f64,
+    pub y: f64
+}
+
+use egui::plot::PlotPoints;
 
 #[derive(Default)]
 pub struct ProjectileData{
@@ -22,7 +24,24 @@ pub struct ProjectileData{
 
 impl ProjectileData{
     //Function describing changes in x's velocity over time.
-    pub fn Fdvxdt(&self) -> f64 {
-        -(self.air_ressitance_coefficient / self.mass) * self.x_velocity * (self.x_velocity)
+    pub fn f_dvx_dt(&self) -> f64 {
+        -(self.air_ressitance_coefficient / self.mass) * self.x_velocity * (self.x_velocity.powi(2) + self.y_velocity.powi(2))
+    }
+
+    pub fn f_dvy_dt(&self) -> f64 {
+        -(self.air_ressitance_coefficient / self.mass) * self.x_velocity * (self.x_velocity.powi(2) + self.y_velocity.powi(2)) -self.gravity
+    }
+
+    pub fn euler_method(&mut self) -> Point {
+        self.x_position = self.x_position + self.step_size * self.x_velocity;
+		self.y_position = self.y_position + self.step_size * self.y_velocity;
+
+		let x_temp_velocity = self.x_velocity + self.f_dvx_dt() * self.step_size;
+		let y_temp_velocity = self.y_velocity + self.f_dvy_dt() * self.step_size;
+
+        self.x_velocity = x_temp_velocity;
+        self.y_velocity = y_temp_velocity;
+
+        Point {x: self.x_position, y: self.y_position}
     }
 }
